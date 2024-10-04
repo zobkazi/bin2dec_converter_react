327ba6ac5f1d5107c685c59637f67636022ac72b
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-04 21:26:13 +0600

    rm layout is dane

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,10 @@
-import NavBar from './components/ui/NavBar.jsx';
-import AllRoutes from './routes.jsx';
-import Layout from './pages/Layout.jsx';
+
+
 const App = () => {
     return (
         <div className="">
-            {/* <NavBar /> */}
-            <Layout />
+           
+           
             
         </div>
     );
