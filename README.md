9f0cd8c143474d03df518239db664295da71735b
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-05 11:10:31 +0600

    apssp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## bin2dec_converter_react
+
+## To-Do List
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Route, Link,  } from 'wouter';
+import About from '../pages/About'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello Kazi</h1>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+      </nav>
+      {/* <Route path="/" component={Home} /> */}
+       <Route path="/about" component={About} />
+  
+     
+    </div>
   )
 }
 
