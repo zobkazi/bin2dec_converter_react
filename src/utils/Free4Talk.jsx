5f2e3581883d39a9dde442bb3c83b49ef1c66231
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-11 22:59:39 +0600

    Free4Talk

---
+++ src/utils/Free4Talk.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Free4Talk = () => {
+  return (
+    <div>Free4Talk</div>
+  )
+}
+
+export default Free4Talk
---
+++ src/utils/NetMirror.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NetMirror = () => {
+  return (
+    <div>NetMirror</div>
+  )
+}
+
+export default NetMirror
